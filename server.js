e39f89683122914a8b6372459faa115417f24217
guillermO566 <70708081@certus.edu.pe>
2025-11-29 12:31:39 -0500

    actualizado

--- server.js
+++ server.js
@@ -1,29 +1,30 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const path = require("path");
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Servir archivos estáticos (HTML, CSS, IMÁGENES)
+// Carpeta pública
 app.use(express.static("public"));
 
-// Ruta donde llegan las suscripciones
+
 app.post("/suscribir", async (req, res) => {
   const correoCliente = req.body.correo;
 
   let transporter = nodemailer.createTransport({
     service: "gmail",
     auth: {
-      user: "guillermowq76@gmail.com",
-      pass: "ftwkrjtioewmfgda"
+      user: process.env.CORREO_USER,
+      pass: process.env.CORREO_PASS
     }
   });
 
   try {
     await transporter.sendMail({
       from: "Suscripciones 7Soaps",
-      to: "guillermowq76@gmail.com",
+      to: process.env.CORREO_USER,
       subject: "Nueva suscripción",
       text: `Un usuario se suscribió con el correo: ${correoCliente}`
     });
@@ -35,7 +36,6 @@ app.post("/suscribir", async (req, res) => {
   }
 });
 
-// Iniciar servidor
-app.listen(3000, () =>
-  console.log("Servidor corriendo en http://localhost:3000")
-);
+// Puerto dinámico (Render usa su propio puerto)
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Servidor corriendo en puerto " + PORT));
