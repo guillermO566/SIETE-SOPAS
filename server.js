012843aa106df18af49ceac9426c4d31ae14a9ee
guillermO566 <70708081@certus.edu.pe>
2025-11-29 14:13:50 -0500

    nuevo

--- server.js
+++ server.js
@@ -2,15 +2,14 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// Servir archivos estáticos (HTML, CSS, Imágenes)
-app.use(express.static("public")); 
-// O si tus archivos están en la raíz, usa: app.use(express.static(__dirname));
 
-// Ruta principal
+app.use(express.static("public")); 
+
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-// Iniciar servidor
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor web listo en puerto " + PORT));
